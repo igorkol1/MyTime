697931b6b551f3388c760d4dc47ed02ac1113559
Igor Kolodziejczyk (IKL) <IKL@kmd.dk>
2019-03-29 19:49:48 +0100

    MyTime-backedn as a spring application

---
+++ MyTime-backend/src/main/java/MyTimebackend/MyTimebackend/MyTimeBackendApplication.java
@@ -0,0 +1,13 @@
+package MyTimebackend.MyTimebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTimeBackendApplication.class, args);
+	}
+
+}
