fd5b1505e89ddcad4943e5fe94efe94e752f9dc9
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-04-03 20:50:11 +0200

    Endpoint to create activity

--- MyTime-backend/src/main/java/MyTimebackend/MyTimebackend/controllers/ActivityController.java
+++ MyTime-backend/src/main/java/MyTimebackend/MyTimebackend/controllers/ActivityController.java
@@ -3,11 +3,11 @@ package MyTimebackend.MyTimebackend.controllers;
 import MyTimebackend.MyTimebackend.domain.entities.ActivityEntity;
 import MyTimebackend.MyTimebackend.services.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,7 +18,17 @@ public class ActivityController {
     private ActivityService activityService;
 
     @GetMapping("/activities/{id}")
-    public List<ActivityEntity> getAllActivities(@PathVariable long id){
+    public List<ActivityEntity> getAllActivities(@PathVariable long id) {
         return activityService.findByUserId(id);
+    }
+
+    @PostMapping("/activities/{userId}/addActivity")
+    public ResponseEntity<Void> saveActivity(@PathVariable long userId, @RequestBody ActivityEntity activity) {
+        ActivityEntity createdActivity = activityService.createActivity(userId, activity);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdActivity.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- MyTime-backend/src/main/java/MyTimebackend/MyTimebackend/services/ActivityService.java
+++ MyTime-backend/src/main/java/MyTimebackend/MyTimebackend/services/ActivityService.java
@@ -3,8 +3,13 @@ package MyTimebackend.MyTimebackend.services;
 import MyTimebackend.MyTimebackend.domain.entities.ActivityEntity;
 import MyTimebackend.MyTimebackend.domain.reporitories.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
@@ -13,8 +18,12 @@ public class ActivityService {
     @Autowired
     ActivityRepository activityRepository;
 
-    public List<ActivityEntity> findByUserId(Long id){
+    public List<ActivityEntity> findByUserId(Long id) {
         return activityRepository.findByUserId(id);
     }
 
+    public ActivityEntity createActivity(long userId, ActivityEntity activity) {
+        activity.setUserId(userId);
+        return activityRepository.save(activity);
+    }
 }
