999b62c44f502ea858ccc3f8f5ef019a688ae51c
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-04-19 12:45:35 +0200

    Add styling to login component

--- MyTime-frontend/src/app/components/login/login.component.ts
+++ MyTime-frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  login() {
+    this.router.navigate(['/activitiesList']);
+  }
+
+  clearFields() {
+    this.username = '';
+    this.password = '';
+  }
 }
